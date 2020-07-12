e6cd3ead34ade5cc391bcaaa8cd2b1163783cfe4
JarrodNB <jnb.cb38@gmail.com>
2020-07-12 17:56:13 -0400

    Set up production for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://frozen-gorge-38819.herokuapp.com/cable" 
 end
